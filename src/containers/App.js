cb2c777d9a4dab96a30936bfca6cf8e94eb059a6
Mac <mac@admins-MacBook-Pro.local>
2022-03-22 21:27:22 +0500

    reusable components developed

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import './App.css';
+import logo from './logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo} />
+    </div>
+  );
+}
+
+export default App;
